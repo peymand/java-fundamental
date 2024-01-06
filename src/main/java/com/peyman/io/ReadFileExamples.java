5fa5e01f8e31559d68df1de7dab87844cf7fdc7d
peyman dodangeh <peymandodangeh@gmail.com>
2024-01-06 12:04:16 +0330

    adding file utilities

---
+++ src/main/java/com/peyman/io/ReadFileExamples.java
@@ -0,0 +1,24 @@
+package com.peyman.io;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+public class ReadFileExamples {
+	// FileOutputStream is meant for writing streams of raw bytes such as image
+	// data.
+	// For writing streams of characters, consider using FileWriter.
+	public void readFileWithFileInputStream() {
+		File file = new File("sample.txt");
+		try (FileInputStream fis = new FileInputStream(file)) {
+			System.out.println("Total file size to read (in bytes) : "+ fis.available());
+			int content;
+			while ((content = fis.read()) != -1) {
+				// convert to char and display it
+				System.out.print((char) content);
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+}

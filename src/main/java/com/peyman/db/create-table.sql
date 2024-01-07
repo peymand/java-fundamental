464d1ad4c10a7680c82c5617ef54366a3342376c
peyman dodangeh <peymandodangeh@gmail.com>
2024-01-07 21:48:15 +0330

    adding content for jdbc section

---
+++ src/main/java/com/peyman/db/create-table.sql
@@ -0,0 +1,1 @@
+create table PERSON (ID bigint not null, EMAIL varchar(255), FIRST_NAME varchar(255), JOINED_DATE date, LAST_NAME varchar(255), primary key (id))
---
+++ src/main/java/com/peyman/db/helpers/JDBCConstants.java
@@ -0,0 +1,16 @@
+package com.peyman.db.helpers;
+
+/**
+ * Change the driver,url,username and password according to your needs
+ * 
+ * @author peymand
+ */
+public class JDBCConstants
+{
+
+   public static final String DRIVER_NAME = "org.hsqldb.jdbc.JDBCDriver";
+   public static final String URL         = "jdbc:hsqldb:hsql://localhost/";
+   public static final String USERNAME    = "root";
+   public static final String PASSWORD    = "123456";
+
+}
